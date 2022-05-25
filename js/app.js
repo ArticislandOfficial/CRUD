92cee4a849612b8dbfb388a426ea889ecd48a37f
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-24 20:31:54 -0500

    se agrega archivo de JS

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let nombre=document.getElementById('nombre').value;
+let email=document.getElementById('email').value;
+let mensaje=document.getElementById('mensaje').value;
+
+
+const agregaTarea = () => {
+    console.log('holi');
+};
