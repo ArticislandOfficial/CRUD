4ac880715df88256fac73b9a6b8e0a3923bed93c
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-25 23:28:09 -0500

    Se crea V1 de CRUD

---
+++ CRUDV1/js/app.js
@@ -0,0 +1,8 @@
+let nombre=document.getElementById('nombre');
+let email=document.getElementById('email');
+let mensaje=document.getElementById('mensaje');
+
+
+const agregaTarea = () => {
+    console.log('holi');
+};
--- js/app.js
+++
@@ -1,8 +0,0 @@
-let nombre=document.getElementById('nombre').value;
-let email=document.getElementById('email').value;
-let mensaje=document.getElementById('mensaje').value;
-
-
-const agregaTarea = () => {
-    console.log('holi');
-};
