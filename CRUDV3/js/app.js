e7ecaa2eb124c256ff124474c3720577299b2d65
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-28 19:59:25 -0500

    Actualizacion de V3

--- CRUDV3/js/app.js
+++ CRUDV3/js/app.js
@@ -2,8 +2,8 @@
 //variables globales
 const formularioUI = document.querySelector("#formulario");
 const listaActividadesUI = document.querySelector("#listaActividades");
-const actividadUI = document.querySelector("#actividad").value;
-let arrayActividades = [];
+// const actividadUI = document.querySelector("#actividad").value;
+let arrayActividades = [];//reinicia arreglo meter en una funcion 1
 
 //funciones
 const CrearItem = (actividad) => {
@@ -14,54 +14,42 @@ const CrearItem = (actividad) => {
   arrayActividades.push(item);
   return item;
 };
+//guardando en local 
 const GuardarDB = () => {
   //KW rutina
   localStorage.setItem("rutina", JSON.stringify(arrayActividades));
   console.log(arrayActividades);
-
+ pintarDB()
 };
 const pintarDB = () => {
-listaActividadesUI.innerHTML = `
-<div class="alert alert-danger" role="alert">
-          <i class="material-icons float-left mr-3"> 
-            accessibility </i>
-          <b>${arrayActividades.actividadUI}</b> - Estado
-          <span class="float-right">
-            <i class="material-icons"> done </i>
-            <i class="material-icons"> delete </i>
-          </span>
-        </div>
-      </div>
-    </div>
-
-`;
-
+listaActividadesUI.innerHTML += `<div id="listaActividades " class="mt-4">     
+          <div class="alert alert-danger" role="alert">
+            <i class="material-icons float-left mr-3"> 
+              accessibility </i>
+            <b>Texto de la actividad</b> - Estado
+            <span class="float-right">
+              <i class="material-icons"> 
+                done 
+              </i>
+              <i class="material-icons"> 
+                delete 
+              </i>
+            </span>
+          </div>
+          
+        </div>`;
 }
 //EventeListener
 
 formularioUI.addEventListener("submit", (e) => {
   //preventDefault evita que refresque o haga cosas raras "XD"
+  
   e.preventDefault();
-  const actividadUI = document.querySelector("#actividad").value;
+  let actividadUI = document.querySelector("#actividad").value;
 //   console.log(actividadUI);
   CrearItem(actividadUI);
   GuardarDB();
-//   formularioUI.reset();
+ formularioUI.reset();
  
 });
-//  document.addEventListener('DOMContentLoaded', pintarDB);
-// if (typeof listaActividadesUi !== "undefined") {
-// Ahora sabemos que foo está definido, ahora podemos continuar.
-// }
-// if (document.readyState !== "loading") {
-//   console.log("document is already ready, just execute code here");
-//   myInitCode();
-// } else {
-//   document.addEventListener("DOMContentLoaded", function () {
-//
-//     console.log("document was not ready, place code here");
-//     myInitCode();
-//   });
-// }
-
-// function myInitCode() {}
+  document.addEventListener('DOMContentLoaded', pintarDB);
