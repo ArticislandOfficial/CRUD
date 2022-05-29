fe8670c294e784215e0fa45ab1a60ba4f45a0692
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-05-28 19:58:51 -0500

    actualizaciones version 1

--- CRUDV1/js/app.js
+++ CRUDV1/js/app.js
@@ -1,8 +1,26 @@
-let nombre=document.getElementById('nombre');
-let email=document.getElementById('email');
-let mensaje=document.getElementById('mensaje');
+const nombre=document.getElementById('nombre');
+const email=document.getElementById('email');
+const mensaje=document.getElementById('mensaje');
+const arrayLs =[];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("boton cliqueado XD");
+  formValidation();
+});
 
 
-const agregaTarea = () => {
-    console.log('holi');
-};
+
+//validacion de formulario 
+let formValidation = ( )=>{
+    if (nombre.value === "" || email.value === "" || mensaje === "") {
+    msg.innerHTML = "tarea en blanco , no se puede agregar";
+    } else {
+      console.log("successss");
+      msg.innerHTML = "";
+    }
+
+    }
+  
+
+
